b7ce3627b26ec81cd96b77103ff7c967942331ca
tpmac1990 <terence.paulmac@gmail.com>
2021-01-17 22:19:12 +1100

    initial commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This is a todo list. It allows you to lists tasks, tick them on completion and delete them when they are of no more use.</p>
+    </React.Fragment>
+  )
+}
+
+
+export default About;
